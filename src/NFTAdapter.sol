901a2f8c0d9dfed5e5d306ea658a2fdad8a94cfc
Lorenzo Manacorda <lorenzo@mailbox.org>
2019-01-18 19:10:44 +0100

    Implement NFTAdapter

--- src/NFTAdapter.sol
+++ src/NFTAdapter.sol
@@ -1,4 +1,52 @@
-pragma solidity ^0.4.24;
+/// NFTAdapter.sol -- Reference EIP-721-compliant non-fungible token adapter
 
-contract NFTAdapter {
+// Copyright (C) 2019 Lorenzo Manacorda <lorenzo@mailbox.org>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >=0.5.0;
+
+import "ds-note/note.sol";
+
+contract NFTAdapter is DSNote {
+    VatLike public vat;
+    bytes32 public ilk;
+    GemLike public gem;
+
+    int256 constant ONE = 10 ** 45;
+
+    constructor(address vat_, bytes32 ilk_, address gem_) public {
+        vat = VatLike(vat_);
+        ilk = ilk_;
+        gem = GemLike(gem_);
+    }
+
+    function join(bytes32 urn, uint256 obj) external note {
+        gem.transferFrom(msg.sender, address(this), obj);
+        vat.slip(ilk, urn, ONE);
+    }
+
+    function exit(bytes32 urn, uint256 obj) external note {
+        gem.transferFrom(address(this), msg.sender, obj);
+        vat.slip(ilk, urn, -ONE);
+    }
+}
+
+contract VatLike {
+    function slip(bytes32,bytes32,int256) public;
+}
+
+contract GemLike {
+    function transferFrom(address,address,uint256) external payable;
 }
