9ed89763c7f0f4f68f6d95bebd1be42bda3f7d38
Lorenzo Manacorda <lorenzo@mailbox.org>
2019-01-19 16:35:31 +0100

    NFTAdapter test

--- src/NFTAdapter.t.sol
+++ src/NFTAdapter.t.sol
@@ -1,21 +1,80 @@
-pragma solidity ^0.4.24;
+/// NFTAdapter.sol -- Reference EIP-721-compliant non-fungible token adapter
+
+// Copyright (C) 2019 Lorenzo Manacorda <lorenzo@mailbox.org>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >=0.5.0;
 
 import "ds-test/test.sol";
 
-import "./NFTAdapter.sol";
+import { GemLike, NFTAdapter } from "./NFTAdapter.sol";
+import { Vat } from "dss/tune.sol";
+import { ERC721Mintable } from "./test/openzeppelin-solidity/token/ERC721/ERC721Mintable.sol";
+
+contract TestUser {
+    ERC721Mintable nft;
+    NFTAdapter     ptr;
+
+    constructor(ERC721Mintable nft_, NFTAdapter ptr_) public {
+        nft = nft_;
+        ptr = ptr_;
+    }
+
+    function approve(address to, uint256 tokenId) public {
+        nft.approve(to, tokenId);
+    }
+
+    function join(uint256 tokenId) public {
+        ptr.join(bytes32(bytes20(address(this))), tokenId);
+    }
+}
 
 contract NFTAdapterTest is DSTest {
-    NFTAdapter adapter;
+    ERC721Mintable nft;
+    GemLike        gem;
+    NFTAdapter     ptr;
+    TestUser       usr;
+    Vat            vat;
+
+    bytes32 constant ilk     = "nft";
+    uint256 constant ONE     = 10 ** 45;
+    uint256 constant tokenId = 42;
 
     function setUp() public {
-        adapter = new NFTAdapter();
+        vat = new Vat();
+        vat.init(ilk);
+
+        nft = new ERC721Mintable();
+        gem = GemLike(address(nft));
+        ptr = new NFTAdapter(address(vat), ilk, address(gem));
+        usr = new TestUser(nft, ptr);
+
+        vat.rely(address(ptr));
+        nft.mint(address(usr), tokenId);
+        usr.approve(address(ptr), tokenId);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
+    function test_balance() public {
+        assertEq(nft.balanceOf(address(usr)), 1);
+        assertEq(nft.balanceOf(address(ptr)), 0);
+        assertEq(vat.gem(ilk, bytes32(bytes20(address(usr)))), 0);
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+        usr.join(tokenId);
+
+        assertEq(nft.balanceOf(address(usr)), 0);
+        assertEq(nft.balanceOf(address(ptr)), 1);
+        assertEq(vat.gem(ilk, bytes32(bytes20(address(usr)))), ONE);
     }
 }
